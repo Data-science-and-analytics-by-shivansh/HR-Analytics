bd4eaa9baf64b2698959e5b08f9fa4bd1507ca13
Shivansh <shivanshpatra.patra@gmail.com>
2026-02-18 23:17:51 +0530

    create_table.sql

---
+++ create_table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Employees (
+    EmpID VARCHAR(20) PRIMARY KEY,
+    Age INT,
+    AgeGroup VARCHAR(10),
+    Attrition VARCHAR(5),
+    Department VARCHAR(50),
+    Gender VARCHAR(10),
+    MonthlyIncome DECIMAL(10,2),
+    JobRole VARCHAR(100),
+    YearsAtCompany INT,
+    OverTime VARCHAR(5),
+    WorkLifeBalance INT,
+    JobSatisfaction INT,
+    PerformanceRating INT,
+    DiversityFlag VARCHAR(20)
+);
